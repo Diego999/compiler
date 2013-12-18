3d8a4d4f4bff3cce6272f34928b8363bdc431d01
Diego Antognini <diegoantognini@gmail.com>
2013-12-18 11:16:23 +0100

    Fix bug no line

--- lex_1.py
+++ lex_1.py
@@ -31,75 +31,92 @@ tokens = (
     "BY_STEP",
     "WHILE"
 
-)+ tuple(map(lambda s:s.upper(),reserved_words_upper)) + tuple(map(lambda s:s.lower(),reserved_words_lower))
+)+ tuple(map(lambda s: s.upper(), reserved_words_upper)) + tuple(map(lambda s: s.lower(), reserved_words_lower))
 
 literals = '();{}'
 
 error_output = open('outputs/' + 'error_lex.log', 'w')
 
+
 def t_NUMBER(t):
     r'\d+(,\d*)*'
     return t
+
 
 def t_STRING(t):
     r'".*"'
     return t
 
+
 def t_BOOL(t):
     r'(vrai)|(faux)'
     return t
+
 
 def t_SMALLER_THAN(t):
     r'plus\spetit\sque'
     return t
 
+
 def t_SMALLER_EQUAL_THAN(t):
     r'plus\spetit\sou\segal\sque'
     return t
+
 
 def t_GREATER_THAN(t):
     r'plus\sgrand\sque'
     return t
 
+
 def t_GREATER_EQUAL_THAN(t):
     r'plus\sgrand\sou\segal\sque'
     return t
+
 
 def t_PLUS_OP(t):
     r'plus'
     return t
 
+
 def t_MINUS_OP(t):
     r'moins'
     return t
+
 
 def t_TIME_OP(t):
     r'fois'
     return t
 
+
 def t_DIVIDE_OP(t):
     r'divise\spar'
     return t
+
 
 def t_EQUAL(t):
     r'est\segal\sa'
     return t
 
+
 def t_ASSIGN(t):
     r'egal'
     return t
+
 
 def t_TYPE(t):
     r'(entier)|(reel)|(texte)|(booleen)'
     return t
 
+
 def t_BY_STEP(t):
     r'par\spas\sde'
     return t
 
+
 def t_WHILE(t):
     r'TANT\sQUE'
     return t
+
 
 def t_IDENTIFIER(t):
     r'[a-zA-Z]+'
@@ -108,21 +125,22 @@ def t_IDENTIFIER(t):
     if t.value.lower() in reserved_words_lower:
         t.type = t.value.lower()
     return t
-       
+
+
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
 t_ignore = '\t '
 
+
 def t_error(t):
     error_output.write("Illegal character '%s'\n" % t.value[0])
     t.lexer.skip(1)
 
-lex.lex()
-
 
 def generate_lex(prog):
+    lex.lex()
     lex.input(prog)
 
 
